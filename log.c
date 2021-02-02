61910f22f90dfc6f6ff4c1939d2abffd436414af
WhiteMask <1049806421@qq.com>
2021-02-02 22:39:18 +0800

    commit

---
+++ log.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include "RH_Utility.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+	void __logMessage(const char* message, ...)
+	{
+
+}
