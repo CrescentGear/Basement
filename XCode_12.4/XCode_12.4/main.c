be712867e72abc12876b0738096b714a8481d4eb
RandleH <40454051+RandleH@users.noreply.github.com>
2021-02-02 18:32:17 +0800

    Add XCode IDE Configuration

---
+++ XCode_12.4/XCode_12.4/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  XCode_12.4
+//
+//  Created by Randle.Helmslay on 2021/2/2.
+//
+
+#include <stdio.h>
+#include "RH_Utility.h"
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+    
+}
